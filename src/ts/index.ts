991ffc1771ba7226ed589dcae6c1c86f41518fb3
NuclearRedeye <github@mneah.com>
2023-05-07 08:56:26 +0100

    feat: add an initial main loop

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,5 +1,29 @@
-import { createTextElement } from './utils/utils.js';
+function onTick(timestamp: number): void {
+  const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+  if (canvas) {
+    const context = canvas.getContext('2d', { alpha: false }) as CanvasRenderingContext2D;
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+  }
+  window.requestAnimationFrame(onTick);
+}
+
+window.onkeydown = (event: KeyboardEvent): void => {
+  switch (event.code) {
+    default:
+      console.info(`onKeyDown(): code = ${event.code}`);
+      break;
+  }
+};
+
+window.onkeyup = (event: KeyboardEvent): void => {
+  switch (event.code) {
+    default:
+      console.info(`onKeyUp(): code = ${event.code}`);
+      break;
+  }
+};
 
 window.onload = function (): void {
-  document.body.appendChild(createTextElement('Created by NuclearRedeye'));
+  window.requestAnimationFrame(onTick);
 };
